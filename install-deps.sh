1d55fe571126995608d00928651a721598ce6cf4
Nicola <nicola.dalmaso@outlook.com>
2017-01-13 13:38:15 +0100

    Modularizzata fase before_install (script installazione dipendenze)

---
+++ install-deps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get -qq update
+
+sudo apt-get -qq install -y --no-install-recommends texlive-fonts-recommended texlive-latex-extra texlive-fonts-extra dvipng texlive-latex-recommended texlive-lang-english texlive-lang-italian latex-xcolor pgf
