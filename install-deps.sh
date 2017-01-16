adf3eb2728784cfa4b7bcc9a0d942a961cc752e5
Nicola <nicola.dalmaso@outlook.com>
2017-01-16 09:47:39 +0100

    Configurazione comando latexmk Tolta installazione pacchetto texlive-full per velocizzare la build in Travis.

--- install-deps.sh
+++ install-deps.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get -qq update
 
-sudo apt-get -qq install -y --no-install-recommends texlive-fonts-recommended texlive-latex-extra texlive-fonts-extra dvipng texlive-latex-recommended texlive-lang-english texlive-lang-italian latex-xcolor pgf lmodern
+sudo apt-get -qq install -y --no-install-recommends texlive texlive-latex-recommended texlive-latex-extra texlive-lang-english texlive-lang-italian latex-xcolor pgf lmodern latexmk
--- travis-build-latex.sh
+++ travis-build-latex.sh
@@ -11,8 +11,7 @@ input_path=$1
 document_name=$2
 build_output_directory=$parent/$3
 
-
 cd $input_path
 
-# comando di build latex, parametri specificati
-pdflatex -interaction=nonstopmode -halt-on-error -output-directory $build_output_directory $input_path/$document_name
+# comando di build latex http://stackoverflow.com/questions/3863630/latex-tableofcontents-command-always-shows-blank-contents-on-first-build
+latexmk -diagnostics -halt-on-error -outdir=$build_output_directory -pdf $document_name
